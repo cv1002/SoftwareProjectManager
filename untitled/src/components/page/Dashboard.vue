d0f3f63ea6775a6ff3b5f2a31e7effe2abb9ec78
OgiwaraSayu <52630736+OgiwaraSayu@users.noreply.github.com>
2020-12-30 10:58:53 +0800

    component_update

--- untitled/src/components/page/Dashboard.vue
+++ untitled/src/components/page/Dashboard.vue
@@ -40,8 +40,8 @@
               <div class="grid-content grid-con-1">
                 <i class="el-icon-lx-people grid-con-icon"></i>
                 <div class="grid-cont-right">
-                  <div class="grid-num">1234</div>
-                  <div>用户访问量</div>
+                  <div class="grid-num">{{numberofmembers}}</div>
+                  <div>小组成员数</div>
                 </div>
               </div>
             </el-card>
@@ -51,8 +51,8 @@
               <div class="grid-content grid-con-2">
                 <i class="el-icon-lx-notice grid-con-icon"></i>
                 <div class="grid-cont-right">
-                  <div class="grid-num">321</div>
-                  <div>系统消息</div>
+                  <div class="grid-num">{{finishedtask}}</div>
+                  <div>已完成任务数</div>
                 </div>
               </div>
             </el-card>
@@ -62,8 +62,8 @@
               <div class="grid-content grid-con-3">
                 <i class="el-icon-lx-goods grid-con-icon"></i>
                 <div class="grid-cont-right">
-                  <div class="grid-num">5000</div>
-                  <div>数量</div>
+                  <div class="grid-num">{{unfinishedtask}}</div>
+                  <div>未完成任务数</div>
                 </div>
               </div>
             </el-card>
@@ -121,6 +121,9 @@ export default {
   name: 'dashboard',
   data() {
     return {
+      numberofmembers:7,
+      finishedtask:12,
+      unfinishedtask:18,
       name: localStorage.getItem('ms_username'),
       todoList: [
         {
