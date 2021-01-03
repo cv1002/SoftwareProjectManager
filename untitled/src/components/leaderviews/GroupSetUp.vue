0e02c59b151ef98a2f05133708184d5504fddea1
ucalan <56287993+ucalan@users.noreply.github.com>
2021-01-03 17:42:00 +0800

    Revert "Merge branch 'pr/8' into main"
    This reverts commit b5371d46c67401f35cafd626ca75adba2965a2f9, reversing
    changes made to 458c7149c7a8e0505345a836b4e131463caf313b.

--- untitled/src/components/leaderviews/GroupSetUp.vue
+++ untitled/src/components/leaderviews/GroupSetUp.vue
@@ -4,21 +4,25 @@
     </div>
     <div class="div3">
       <el-form ref="form" :model="form" label-width="80px">
-        <el-form-item label="组名" style="width: 37%">
-          <el-input v-model="form.name"></el-input>
+        <el-form-item label="组名" style="width:296px;margin-bottom: 30px">
+          <el-input v-model="form.groupname"></el-input>
         </el-form-item>
-        <el-form-item label="选题">
-          <el-select v-model="form.region" placeholder="请选择题目" style="width: 30%">
-            <el-option label="题一" value="题目1"></el-option>
+        <el-form-item label="选题" style="width: 800px;margin-bottom: 30px">
+          <el-select v-model="form.subject" placeholder="请选择题目" style="width: 30%" @change="getChange(form.subject)">
+            <el-option label="项目管理系统" value="项目管理系统"></el-option>
+            <el-option label="游戏" value="游戏"></el-option>
+            <el-option label="操作系统" value="操作系统"></el-option>
             <el-option label="其他" value="其他"></el-option>
           </el-select>
         </el-form-item>
-        <el-form-item label="项目时间">
+        <el-form-item v-if="subjectVisible" label="自拟题目" style="width: 430px;margin-bottom: 30px">
+          <el-input v-model="form.desc" type="textarea"></el-input>
+        </el-form-item>
+        <el-form-item label="项目时间" style="margin-bottom: 30px">
           <el-col :span="11">
             <el-date-picker
                 v-model="form.value2"
                 :picker-options="pickerOptions"
-                align="right"
                 end-placeholder="结束日期"
                 format="yyyy 年 MM 月 dd 日"
                 range-separator="至"
@@ -30,30 +34,28 @@
           </el-col>
         </el-form-item>
 
-        <el-form-item label="项目类型">
-          <el-checkbox-group v-model="form.type">
-            <el-checkbox label="微信小程序" name="type"></el-checkbox>
-            <el-checkbox label="网页" name="type"></el-checkbox>
-            <el-checkbox label="游戏" name="type"></el-checkbox>
-            <el-checkbox label="其他" name="type"></el-checkbox>
-          </el-checkbox-group>
+        <el-form-item label="项目类型" style="margin-bottom: 30px">
+          <el-radio-group v-model="form.type">
+            <el-radio label="微信小程序" name="type"></el-radio>
+            <el-radio label="网页" name="type"></el-radio>
+            <el-radio label="游戏" name="type"></el-radio>
+            <el-radio label="其他" name="type"></el-radio>
+          </el-radio-group>
         </el-form-item>
-        <el-form-item label="自拟题目" style="width: 80%">
-          <el-input v-model="form.desc" type="textarea" ></el-input>
-        </el-form-item>
+
         <el-form-item>
           <el-button type="primary" @click="onSubmit">立即创建</el-button>
-          <el-button>取消</el-button>
+          <el-button>重置</el-button>
         </el-form-item>
       </el-form>
       <div class="block" style="margin-left: 50px;margin-top: 50px">
         <el-timeline>
           <el-timeline-item
               v-for="(activity, index) in activities"
-              :color="activity.color"
               :key="index"
+              :color="activity.color"
               :timestamp="activity.timestamp">
-            {{activity.content}}
+            {{ activity.content }}
           </el-timeline-item>
         </el-timeline>
       </div>
@@ -65,12 +67,13 @@
 export default {
   data() {
     return {
+      subjectVisible: false,
       form: {
-        name: '',
-        region: '',
-        type: [],
+        groupname: '',
+        subject: '', //题目
+        type: '',
         desc: '',
-        value2:''
+        value2: ''
       },
       pickerOptions: {
         shortcuts: [{
@@ -102,11 +105,11 @@ export default {
       activities: [{
         content: '小组选题',
         timestamp: '2018-04-11',
-        color: 'lightskyblue',
+        color: 'lightskyblue'
       }, {
         content: '审核中',
         timestamp: '2018-04-13',
-        color: 'white',
+        color: 'white'
       }, {
         content: '创建成功',
         timestamp: '2018-04-15',
@@ -116,16 +119,27 @@ export default {
   },
   methods: {
     onSubmit() {
-      console.log(this.form.name);
-      console.log(this.form.region);
+      console.log(this.form.groupname);
       console.log(this.form.value2[0]);
       console.log(this.form.value2[1]);
-      console.log(this.form.type[0]);
-      console.log(this.form.desc);
+      console.log(this.form.type);
+      if (this.form.subject !== '其他') {
+        console.log(this.form.subject);
+      } else {
+        console.log(this.form.desc);
+      }
       this.$cookie.set('groupsetenable', false);
       this.activities[0].color = 'white';
       this.activities[1].color = 'lightskyblue';
       console.log('submit!');
+    },
+    getChange(item) {
+      console.log(item);
+      if (item === '其他') {
+        this.subjectVisible = true;
+      } else {
+        this.subjectVisible = false;
+      }
     }
   }
 };
